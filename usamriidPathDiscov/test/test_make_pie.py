6afaaa8eed0ac6a9adb41b0ddc802edfbde1b46a
Tyghe Vallard <vallardt@gmail.com>
2015-02-11 14:57:13 -0500

    Fixing how make_pie figures out what label to use when counting. Also added some small tests to make sure things are working

---
+++ usamriidPathDiscov/test/test_make_pie.py
@@ -0,0 +1,87 @@
+import unittest2 as unittest
+import mock
+
+from .. import make_pie
+
+PHYLO = '''taxid	count	superkingdom	kingdom	class	order	family	genus	species	descrip
+1\t1.0\tBacteria\tKingdom\tClass\tOrder\tFamily\tGenus\tBact1\tDescription
+1\t1.0\tBacteria\tBact2\t-\t-\t-\t-\t-\tDescription
+1\t1.0\tViruses\tKingdom\tClass\tOrder\tFamily\tGenus\tVirus1\tDescription
+1\t1.0\tViruses\t-\t-\t-\t-\t-\t-\tVirus2
+1\t1.0\tWhatever\tKingdom\tMammalia\tOrder\tFamily\tGenus\tMammal1\tDescription
+1\t1.0\tWhatever\tKingdom\tInsecta\tOrder\tFamily\tGenus\tInsecta1\tDescription
+'''
+
+class TestFindBestName(unittest.TestCase):
+    def setUp(self):
+        self.phylo_headers = [
+            'taxid', 'count', 'superkingdom',
+            'kingdom', 'class', 'order',
+            'family', 'genus', 'species',
+            'descrip'
+        ]
+
+    def test_uses_species(self):
+        cols = self.phylo_headers
+        r = make_pie.find_best_name(cols)
+        self.assertEqual(
+            cols[8],
+            r
+        )
+
+    def test_works_backwords_and_finds_non_dash(self):
+        # Make all -
+        cols = ['-' for i in range(len(self.phylo_headers))]
+        cols[2] = 'Superkingdom'
+        r = make_pie.find_best_name(cols)
+        self.assertEqual(
+            cols[2],
+            r
+        )
+
+    def test_falls_back_on_description(self):
+        # Make all -
+        cols = ['-' for i in range(len(self.phylo_headers))]
+        cols[-1] = 'description'
+        r = make_pie.find_best_name(cols)
+        self.assertEqual(
+            cols[-1],
+            r
+        )
+
+    def test_raises_valueerror_if_none_found(self):
+        # Make all -
+        cols = ['-' for i in range(len(self.phylo_headers))]
+        self.assertRaises(
+            ValueError,
+            make_pie.find_best_name, cols
+        )
+
+class TestHostVectorPathogen(unittest.TestCase):
+    def setUp(self):
+        self.phylo = PHYLO
+
+    @mock.patch('__builtin__.open')
+    def test_correct_calls(self, mopen):
+        mopen.return_value.read.return_value = self.phylo
+        r = make_pie.host_vector_pathogen(['foo.phylo','bar.phylo'])
+        mopen.assert_has_calls([
+            mock.call('foo.phylo'),
+            mock.call().read(),
+            mock.call('bar.phylo'),
+            mock.call().read(),
+        ])
+
+    @mock.patch('__builtin__.open')
+    def test_correct_calls(self, mopen):
+        mopen.return_value.read.return_value = self.phylo
+        r = make_pie.host_vector_pathogen(['foo.phylo','bar.phylo'])
+        self.assertEqual(2.0, r[0]['Mammal1'])
+        self.assertEqual(2, r[1])
+        self.assertEqual(2.0, r[2]['Insecta1'])
+        self.assertEqual(2, r[3])
+        self.assertEqual(2.0, r[4]['Bact1'])
+        self.assertEqual(2.0, r[4]['Bact2'])
+        self.assertEqual(2.0, r[4]['Virus1'])
+        self.assertEqual(2.0, r[4]['Viruses'])
+        self.assertEqual(8, r[5])
