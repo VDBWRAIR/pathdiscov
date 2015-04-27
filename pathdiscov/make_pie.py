e75923c13c4bb5ff617ec55bc9a2c1c1142e06fe
Tyghe Vallard <vallardt@gmail.com>
2015-04-27 16:32:31 -0400

    More fine tuning of the graphic

--- pathdiscov/make_pie.py
+++ pathdiscov/make_pie.py
@@ -86,8 +86,74 @@ def host_vector_pathogen(phylo_files, hostclass, vectorclass, pathogenclass):
 
     return hosts, hosts_ct, vectors, vectors_ct, pathogens, pathogens_ct
 
-def graph_( ax, counts, **kwargs ):
+def graph_(ax1, ax2, counts, **kwargs):
+    '''
+    Plot and pie next to each other
+    Pie only has separate wedges for >5.0 values and the rest are lumped together
+    into 'other'
+
+    ax1 - line plot axes
+    ax2 - pie axes
+    counts - (('label',value),...)
+    **kwargs - kwargs for plot like 'title'
+    '''
+    # Sorting makes the graphic look nicer
     counts = sorted(counts.items(), key=lambda x: x[1], reverse=True)
+    values = [0] + [v for k,v in counts]
+    labels = [''] + ['{k}({v})'.format(k=k,v=v) for k,v in counts]
+    title = kwargs.get('title')
+
+    pievalues = []
+    pielabels = []
+    othercount = 0.0
+    othernames = []
+    for label, count in counts:
+        if count >= 5.0:
+            pievalues.append(count)
+            pielabels.append(label)
+        else:
+            othercount += count
+            othernames.append(label)
+    pievalues.append(othercount)
+    pielabels.append('Other (<5.0)')
+    totalcnt = sum(pievalues)
+
+    ax1.set_xticklabels(labels, rotation=90)
+    ax1.set_xticks(range(len(labels)))
+    ax1.plot(values, 'bo-')
+    ax1.set_title('All {0}'.format(title))
+
+    def my_autopct(pct):
+        val = int(pct * totalcnt / 100.0)
+        return '{p:.2f}% ({v:d})'.format(p=pct,v=val)
+
+    pta = ax2.pie(
+        pievalues,
+        labels=pielabels,
+        autopct=my_autopct,
+        colors=('b', 'g', 'r', 'c', 'm', 'y', 'k'),
+        pctdistance=0.8,
+        wedgeprops={'linewidth':0},
+    )
+    ax2.set_title('Top {0}'.format(title))
+
+    # Try to rotate autotexts
+    for patch, text, autotext in zip(pta[0], pta[1], pta[2]):
+        theta_half = (patch.theta2 - patch.theta1) / 2.0
+        theta = patch.theta1 + theta_half
+        #print theta
+        #print text.get_text()
+        if theta > 90 and theta < 270:
+            theta += 180
+        
+        autotext.set_rotation(theta)
+        autotext.set_color('w')
+
+def graph1_(ax, counts, **kwargs):
+    # Sorting makes the graphic look nicer
+    counts = sorted(counts.items(), key=lambda x: x[1], reverse=True)
+    print counts
+    '''
     unique_counts = defaultdict(list)
     for label, count in counts:
         unique_counts[count].append(label)
@@ -100,7 +166,10 @@ def graph_( ax, counts, **kwargs ):
         if l > 5:
             label = '{0}\n({1} more...)'.format('\n'.join(_labels[0:5]),l)
         labels.append(label)
-    print labels
+    '''
+    ax.set_title(kwargs['title'])
+    cts = [v for k,v in counts]
+    labels = [k for k,v in counts]
 
     def my_autopct(pct):
         total = sum(cts)
@@ -113,16 +182,16 @@ def graph_( ax, counts, **kwargs ):
     #ax.set_xticks(xs)
     #ax.set_xticklabels(labels, rotation=90, ha='left')
 
-def graph_vectors( ax, vector_counts ):
-    graph_( ax, vector_counts, title='Vectors' )
+def graph_vectors(ax1, ax2, vector_counts):
+    graph_(ax1, ax2, vector_counts, title='Vectors' )
 
-def graph_hosts( ax, host_counts ):
-    graph_( ax, host_counts, title='Hosts' )
+def graph_hosts(ax1, ax2, host_counts):
+    graph_(ax1, ax2, host_counts, title='Hosts')
 
-def graph_pathogens( ax, pathogens_counts ):
-    graph_( ax, pathogens_counts, title='Pathogens' )
+def graph_pathogens(ax1, ax2, pathogens_counts ):
+    graph_(ax1, ax2, pathogens_counts, title='Pathogens')
 
-def graph_all( ax, host_vector_pathogen, **kwargs ):
+def graph_all(ax, host_vector_pathogen, **kwargs):
     hc,vc,pc = host_vector_pathogen[1::2]
     hvp = [('Mamalia',hc), ('Insecta',vc), ('Virus/Bacteria',pc)]
     for k,v in hvp:
@@ -145,20 +214,25 @@ def create_image( project_path, **kwargs ):
     fig = plt.figure()
 
     fig.suptitle( basename(kwargs['output_path']) )
-    fig.set_size_inches( 50.0, 10.0 )
+    fig.set_size_inches( 100, 67 )
 
-    gs = gridspec.GridSpec( 1, 4 )
+    ratios = [2,1,2,1,2,1,2]
+    gs = gridspec.GridSpec(1, 7, width_ratios=ratios, height_ratios=ratios)
     ax1 = plt.subplot( gs[0] )
     ax2 = plt.subplot( gs[1] )
     ax3 = plt.subplot( gs[2] )
     ax4 = plt.subplot( gs[3] )
+    ax5 = plt.subplot( gs[4] )
+    ax6 = plt.subplot( gs[5] )
+    ax7 = plt.subplot( gs[6] )
 
-    graph_vectors( ax1, v )
-    graph_hosts( ax2, h )
-    graph_pathogens( ax3, p )
-    graph_all( ax4, hvp, title='Sample Composition' )
+    graph_vectors(ax1, ax2, v)
+    graph_hosts(ax3, ax4, h)
+    graph_pathogens(ax5, ax6, p)
+    graph_all(ax7, hvp, title='Sample Composition')
 
-    gs.tight_layout(fig, w_pad=20)
+    #gs.tight_layout(fig, w_pad=20)
+    fig.tight_layout()
     fig.savefig( kwargs['output_path'], dpi=100, pad_inches=0.1 )
 
 def parse_args( args=sys.argv[1:] ):
