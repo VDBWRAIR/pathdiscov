a6a14e4a94590cfa74986d3975561842bb1dd3fa
Tyghe Vallard <vallardt@gmail.com>
2015-07-16 12:24:12 -0400

    Fixes a leftover bug from prior commit with joinlines

--- pathdiscov/ray2_assembly/joinlines.sh
+++ pathdiscov/ray2_assembly/joinlines.sh
@@ -2,7 +2,7 @@
 
 # replace ids w numbers and fastajoinlines
 
-input=$2		# in
-output=$3		# out
+input=$1		# in
+output=$2		# out
 
 cat ${input} | fastajoinlines | awk 'BEGIN{id=1}{if ($0 ~ /^>/) {print ">c"id; print "c"id"\t"substr($0,2) > "contig.id"; id++;} else {print}}' > ${output}
