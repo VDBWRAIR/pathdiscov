9d3e0e864eef29ce35d38d70d0f8758b48071e92
Tyghe Vallard <vallardt@gmail.com>
2015-07-16 12:24:36 -0400

    Refactoring test code for re-usability

--- tests/common.py
+++ tests/common.py
@@ -274,3 +274,37 @@ class TempDir(unittest.TestCase):
             print "Not removing {0} because keep_temp_dir set".format(self.testdir)
         else:
             shutil.rmtree(self.testdir)
+
+class StageTestBase(unittest.TestCase):
+    @classmethod
+    def setUpClass(klass):
+        klass.tdir = join(SCRATCH, klass.__name__)
+        if exists(klass.tdir):
+            shutil.rmtree(klass.tdir)
+        os.makedirs(klass.tdir)
+        os.chdir(klass.tdir)
+
+    def setUp(self):
+        self.tdir = self.__class__.tdir
+        self.r1_fq = join(TESTDATA, 'F.fastq')
+        self.r2_fq = join(TESTDATA, 'R.fastq')
+
+    @classmethod
+    def tearDownClass(self):
+        os.chdir('/')
+
+    def _write_param(self, txt):
+        with open('param.txt','w') as fh:
+            fh.write(txt)
+        return abspath('param.txt')
+
+    def _verify_countfile(self, expect, countpath):
+        '''
+        expect is list((name,count),(name2,count)...)
+        where each tuple should match a line in countpath file
+        '''
+        fh = open(countpath)
+        for line, ex in zip(fh, expect):
+            name, count = line.strip().split()
+            self.assertEqual(ex[0], name)
+            self.assertEqual(ex[1], count)
--- tests/test_quality_filter.py
+++ tests/test_quality_filter.py
@@ -7,6 +7,7 @@ import unittest2 as unittest
 import sh
 
 from common import TESTDATA, SCRATCH, PATHDISCOV
+import common
 
 # Allows us to reference sh.quality_filter since quality_filter.pl
 # cannot just work with sh import because of .pl
@@ -34,40 +35,7 @@ expect_count_r2 = [
     ('prinseq', '158')
 ]
 
-class TestQualityFilter(unittest.TestCase):
-    @classmethod
-    def setUpClass(klass):
-        klass.tdir = join(SCRATCH, 'quality_filter')
-        if exists(klass.tdir):
-            shutil.rmtree(klass.tdir)
-        os.makedirs(klass.tdir)
-        os.chdir(klass.tdir)
-
-    def setUp(self):
-        self.tdir = TestQualityFilter.tdir
-        self.r1_fq = join(TESTDATA, 'F.fastq')
-        self.r2_fq = join(TESTDATA, 'R.fastq')
-
-    @classmethod
-    def tearDownClass(self):
-        os.chdir('/')
-
-    def _write_param(self, txt):
-        with open('param.txt','w') as fh:
-            fh.write(txt)
-        return abspath('param.txt')
-
-    def _verify_countfile(self, expect, countpath):
-        '''
-        expect is list((name,count),(name2,count)...)
-        where each tuple should match a line in countpath file
-        '''
-        fh = open(countpath)
-        for line, ex in zip(fh, expect):
-            name, count = line.strip().split()
-            self.assertEqual(ex[0], name)
-            self.assertEqual(ex[1], count)
-
+class TestQualityFilter(common.StageTestBase):
     def test_runs_on_r1(self):
         param = self._write_param(paramtxt)
         outdir = 'r1'
