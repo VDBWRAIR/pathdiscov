169380908b276f7cfc890a4548cd2a06a5fd09a6
Tyghe Vallard <vallardt@gmail.com>
2015-08-13 08:07:48 -0400

    Closes #278. Fixes format_iterative_blast_phylo.pl that was broken in previous commit

--- pathdiscov/iterative_blast_phylo/format_iterative_blast_phylo.pl
+++ pathdiscov/iterative_blast_phylo/format_iterative_blast_phylo.pl
@@ -290,8 +290,8 @@ foreach my $mate (@mates)
 		close($outphylo_top);
 
 		# this is hack-ish: cut off space-consuming seq cols to make smaller report
-		print_system("cat $outputdir/$mate.$prefix.report.txt | cut -f2-- > $outputdir/$mate.$prefix.smallreport.txt") if ($bigreport);		# report
-		print_system("cat $outputdir/$mate.$prefix.top.report.txt | cut -f2-- > $outputdir/$mate.$prefix.top.smallreport.txt");			# top hit report		
+		print_system("cat $outputdir/$mate.$prefix.report.txt | cut -f2- > $outputdir/$mate.$prefix.smallreport.txt") if ($bigreport);		# report
+		print_system("cat $outputdir/$mate.$prefix.top.report.txt | cut -f2- > $outputdir/$mate.$prefix.top.smallreport.txt");			# top hit report		
 				
 	} # mate exists
 } # mate
